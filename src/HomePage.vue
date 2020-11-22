da967802a00da89cb0da41323468bbeae8d413a3
Marianne Grimstad <marianne.grimstad@gmail.com>
2020-11-22 20:01:28 +0100

    initial commit

---
+++ src/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="[ row ]">
+    <div class="[ col-sm-12 ]">
+         <recipeComponent
+                    
+         ></recipeComponent>
+       </div>
+    </div>
+</template>
+
+<script>
+import recipeComponent from './components/recipeComponent.vue'
+export default {
+    name: 'HomePage',
+    components:{
+      recipeComponent
+    },
+    data(){
+      return{
+        recipes: []
+      }
+    },
+    beforeMount: function(){
+      const app= this;
+    
+    const conversionUrl = 'https://cors-anywhere.herokuapp.com/';
+    const url = 'http://www.recipepuppy.com/api/';
+      
+        fetch(conversionUrl + url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(result) {
+          app.recipes = result;
+        })
+    }
+    }
+      
+    
+  
+
+</script>
---
+++ src/components/recipeComponent.vue
@@ -0,0 +1,33 @@
+<template>
+        <div class="[ card ]">
+            <div class="[ row ]">
+                 <div class="[ col-sm-4 ]"><img :src="image" /><br /><br /><br /></div>
+                 <div class="[ col-sm-4 ]"></div>
+            </div>
+               <div class="[ card--information ]"><br /> 
+                  
+              </div>
+        </div>   
+</template>
+
+<script>
+export default {
+    name: 'recipeComponent',
+    props: ['image', 'title', 'link', 'ingredients'],
+}
+
+</script>
+
+
+<style scoped>
+.card{
+    border: solid 1px black;
+    border-radius: 4px;
+    padding: 0px 20px 30px;
+    margin-top: 30px;
+}
+
+.card img{
+    width: 100%;
+    padding-top: 20px;
+}
